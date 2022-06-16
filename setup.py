f490840815edc5fd61093148755954e34627f6f8
Francesco Iacovelli <70752491+FrancescoIacovelli@users.noreply.github.com>
2022-06-16 10:58:44 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setuptools.setup(
     license='GNU GPLv3',
     python_requires='>=3.7',
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'jax', 'scipy', 'astropy', 'h5py', 'mpmath', 'matplotlib', 'numdifftools', 'multiprocessing', 'schwimmbad'],
+    install_requires=['numpy', 'jax', 'scipy', 'astropy', 'h5py', 'mpmath', 'matplotlib', 'numdifftools', 'schwimmbad'],
 )
