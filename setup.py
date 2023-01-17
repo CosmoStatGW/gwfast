bebe1c62ba94db91db1e59cf7597621455144ba3
Francesco Iacovelli <70752491+FrancescoIacovelli@users.noreply.github.com>
2023-01-17 19:46:35 +0100

    Added run directory to setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url='https://github.com/CosmoStatGW/gwfast',
     license='GNU GPLv3',
     python_requires='>=3.7',
-    packages=setuptools.find_packages(),
+    packages=['gwfast', 'run'],#setuptools.find_packages(),
     include_package_data=True,
     package_data={'':['../WFfiles/*.txt', '../WFfiles/*.h5', '../psds/*', '../psds/ce_curves/*', '../psds/LVC_O1O2O3/*', '../psds/observing_scenarios_paper/*', '../psds/unofficial_curves_all_dets/*']},
     install_requires=['numpy', 'scipy', 'astropy', 'h5py', 'mpmath', 'matplotlib', 'numdifftools', 'schwimmbad'],
